3064465e1612668d00dd674a606c68a89339e122
javanlacerda <javan.lacerda@ccc.ufcg.edu.br>
2018-04-27 20:18:52 -0300

    implementado sistema de envio de mensagens, mudanças de inteface feitas

--- client.js
+++ client.js
@@ -1,11 +1,54 @@
 const listagem = document.getElementById("listagem");
+var mensagens = [];
 
-function listar (response){
 
+function cadastrar() {
+    var titulo = document.getElementById("title");
+    var mensagem = document.getElementById("mensagem");
+    var autor = document.getElementById("author");
 
-    let mensagens = response.map(msg => `<ul class="card-javan"><li><h5><b>Author:</b> ${msg.author}</li> <li><h5><b>Título:</b> ${msg.title}</li> <li><h5><b>Mensagem:</b> ${msg.msg}</li></ul>`).join(" ");
-    
+    fetch('http://150.165.85.16:9900/api/msgs', {
+        method: 'POST',
+        body: JSON.stringify({
+            title: titulo.value,
+            msg: mensagem.value,
+            author: autor.value,
+            credentials: "javan:j@van123"
+        })
+    });
+    if (title.value.trim() || "" && msg.value.trim() || "" && author.value.trim() != "") {
+
+        update({
+        title: titulo.value,
+        msg: mensagem.value,
+        author: autor.value
+    })} else {
+
+        alert("Necessário preencher todos os campos!")
+    }
+resetForm(titulo, mensagem, autor);
+}
+
+function resetForm(titulo, mensagem, autor) {
+    titulo.value = '';
+    mensagem.value = '';
+    autor.value = '';
+}
+function update(msg) {
+
+    mensagens += `<ul class="card-javan"><li><h5><b>Autor:</b> ${msg.author}</li> <li><h5><b>Título:</b> ${msg.title}</li> <li><h5><b>Mensagem:</b> ${msg.msg}</li></ul>`;
+    listagem.innerHTML = mensagens;
+
+}
+fetch("http://150.165.85.16:9900/api/msgs").then(response => response.json()).then(promisse => listing(promisse));
+
+function listing(response) {
+
+    mensagens = response.map(msg => `<ul class="card-javan"><li><h5><b>Autor:</b> ${msg.author}</li> <li><h5><b>Título:</b> ${msg.title}</li> <li><h5 style="position: relative;"><b>Mensagem:</b> ${msg.msg}</li></ul>`).join(" ");
+
     listagem.innerHTML = mensagens;
 }
 
-fetch("http://150.165.85.16:9900/api/msgs").then(response => response.json()).then(promisse => listar(promisse));
+
+
+
