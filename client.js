36bcbaf1e0b0179dc8af50f839eb089ee1ccefcb
javanlacerda <javan.lacerda@ccc.ufcg.edu.br>
2018-04-29 00:57:21 -0300

    get, post, delete, filter implementados

--- client.js
+++ client.js
@@ -1,6 +1,8 @@
-const listagem = document.getElementById("listagem");
-var mensagens = [];
 
+
+const listagem = document.getElementById("listagem");
+var filtradas = [];
+var res = [];
 
 function cadastrar() {
     var titulo = document.getElementById("title");
@@ -19,36 +21,143 @@ function cadastrar() {
     if (title.value.trim() || "" && msg.value.trim() || "" && author.value.trim() != "") {
 
         update({
-        title: titulo.value,
-        msg: mensagem.value,
-        author: autor.value
-    })} else {
+            title: titulo.value,
+            msg: mensagem.value,
+            author: autor.value
+        })
+    } else {
 
         alert("Necessário preencher todos os campos!")
     }
-resetForm(titulo, mensagem, autor);
+    resetForm();
 }
 
 function resetForm(titulo, mensagem, autor) {
-    titulo.value = '';
-    mensagem.value = '';
-    autor.value = '';
+
+    document.getElementById("title").value = "";
+    document.getElementById("mensagem").value = "";
+    document.getElementById("author").value = "";
+    document.getElementById("deletar").value = "";
+    document.getElementById("filtro").value = "";
 }
+
 function update(msg) {
 
-    mensagens += `<ul class="card-javan"><li><h5><b>Autor:</b> ${msg.author}</li> <li><h5><b>Título:</b> ${msg.title}</li> <li><h5><b>Mensagem:</b> ${msg.msg}</li></ul>`;
-    listagem.innerHTML = mensagens;
+
+    res.unshift
+    (msg);
+    listing(res);
 
 }
-fetch("http://150.165.85.16:9900/api/msgs").then(response => response.json()).then(promisse => listing(promisse));
+fetch("http://150.165.85.16:9900/api/msgs").then(response => response.json()).then(guardaPromisse);
 
 function listing(response) {
 
-    mensagens = response.map(msg => `<ul class="card-javan"><li><h5><b>Autor:</b> ${msg.author}</li> <li><h5><b>Título:</b> ${msg.title}</li> <li><h5 style="position: relative;"><b>Mensagem:</b> ${msg.msg}</li></ul>`).join(" ");
+    response.sort(comparator)
+    response.sort(comparator)
+    response.sort(comparator)
 
+
+
+    mensagens = response.map(msg => '<ul class="card-javan" ' + getRandomInt() + ` >${msg.id}
+    <li style="color: #FFFFFF; padding: 0px 10px;" ><h5  style="font: italic bold 25px Purisa, sans-serif;"><b>front: ${msg.frontend}</b></li>
+
+    <li style="color: #FFFFFF; padding: 0px 10px;" ><h5  style="font: italic bold 25px Purisa, sans-serif;"><b>Por: ${msg.author}</b></li>
+    <li style="color: #FFFFFF;padding: 0px 10px;"><h5 style="font: italic bold 25px Purisa, sans-serif;"><b>Título: ${msg.title}</b></li>
+    <li style="color: #FFFFFF;padding: 0px 20px;"><h5 style="font: italic bold 25px Purisa, sans-serif;"><b>Mensagem:${msg.msg}</b></li></ul>`).join(" ");
+    watch();
     listagem.innerHTML = mensagens;
+
 }
 
+function guardaPromisse(promisse) {
+
+    res = promisse;
+    listing(promisse);
+
+}
+
+function getRandomInt() {
+    min = Math.ceil(0);
+    max = Math.floor(99999);
+    return "style='background-color: #444444;'";
+}
+
+function filtering(value) {
+
+    filtradas = res.filter(msg => verificaTexto(msg.author, value) || verificaTexto(msg.title, value) || verificaTexto(msg.msg, value) || msg.id == value);
+    if (filtradas.length == 0) {
+
+        listagem.innerHTML = "<h2 style='position: relative; text-align: center; color: #FFFFFF'> Nenhum resultado encontrado! </h2>"
+    } else {
+
+        listing(filtradas)
+    }
+}
+
+function verificaTexto(texto, value) {
+
+    const temp = texto.split(" ");
+
+    let fils = temp.filter(element => element.toLowerCase() == value.toLowerCase());
+
+    if (fils.length != 0) return true;
+
+    return false;
+}
+
+function watch() {
+
+    flag = document.getElementById("filtro").value.trim();
+
+    setInterval(function () {
+
+        let campo = document.getElementById("filtro").value.trim();
+        if (campo != "" && campo != flag) {
+            filtering(campo);
+            flag = campo;
+
+        } else if (campo == "" && flag != "") {
+            flag = "";
+            listing(res);
+        }
+
+    }, 100)
 
 
+}
+
+function deletar() {
+
+    const idd = document.getElementById("deletar");
+
+    fetch('http://150.165.85.16:9900/api/msgs/' + idd.value, {
+        method: 'DELETE',
+        body: JSON.stringify({
+            credentials: "javan:j@van123"
+        })
+    });
+    const index = res.map(msg => msg.id).indexOf(idd.value)
+    
+    res.splice(index, 1);   
+    
+    listing(res);
+    resetForm();
+}
+
+function comparator(val1, val2) {
+
+    if (val1.created_at > val2.created_at) {
+
+        return -1;
+    } else if (val1.created_at < val2.created_at) {
+
+        return 1;
+
+    } else {
+
+        return 0;
+    }
+
+}
 
